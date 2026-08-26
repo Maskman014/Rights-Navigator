e5e7e7dc6b88ac59ba9f731804fbcd55dd112108
RadhaKrishna Veernala <veernala.krishna.14@gmail.com>
2026-08-26 13:55:07 +0530

    Convert db.js to TypeScript and update exports

--- api/db.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
-  throw new Error('Please define SUPABASE_URL and SUPABASE_ANON_KEY environment variables inside Vercel.');
-}
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-
-module.exports = supabase;
